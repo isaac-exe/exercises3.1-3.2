5f47ee3757a892aa32d605d9a916e826bea19fc5
Isaac <iscrnldo@gmail.com>
2024-11-27 17:24:37 -0800

    updated port declaration

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.json({id: id})
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
